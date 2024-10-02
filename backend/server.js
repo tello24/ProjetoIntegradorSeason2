eb1336e7bfaf4721743ec7a4d98717631b042d72
matheus-debian <matheus-debian@debian-matheus.matheus>
2024-10-02 12:46:22 -0300

    ADD: GET/POST

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,35 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+
+let user = [
+    {
+        login: "admin",
+        senha: "admin"
+    }
+]
+
+
+app.get('/login', (req, res) => {
+    res.json(user)
+})
+
+app.post('/login', (req, res) => {
+    const login = req.body.login
+    const senha = req.body.senha
+
+    const novo_login = {
+        login: login,
+        senha: senha
+    }
+
+    user.push(novo_login)
+
+    res.json(user)
+})
+
+app.listen(3002, () => {
+    console.log("server up and running")
+})
