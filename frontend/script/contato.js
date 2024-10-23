c7bccfdccd42d2257b09adf1080ce267df82ba4f
matheus-debian <matheus-debian@debian-matheus.matheus>
2024-10-23 11:24:29 -0300

    ADD: LOCATION MAP

--- frontend/script/contato.js
+++ frontend/script/contato.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([-23.64783921317085, -46.57399714073588], 15);
+var map = L.map('map').setView([-23.61995230868037, -46.66903327422016], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-23.64783921317085, -46.57399714073588]).addTo(map);
+var marker = L.marker([-23.61995230868037, -46.66903327422016]).addTo(map);
