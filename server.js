76eaad4fc209ef0633d6965513b72927fada113a
matheus-debian <matheus-debian@debian-matheus.matheus>
2024-11-01 14:21:28 -0300

    ADD: DEPENDENCIES

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 app.use(express.json());
